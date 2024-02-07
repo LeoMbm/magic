9c1e6fc838865880baef56eed00f68736974937a
LeoMbm <leonidas.j.mbm@gmail.com>
2024-02-07 19:47:23 +0100

    fix

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/app/auth";
 
 export async function GET(req: NextRequest) {
   try {
+    const session = await auth();
+    if (!session) {
+      return new Response(JSON.stringify({ message: "Not Authorized" }), {
+        status: 401,
+      });
+    }
     const name = new URL(req.url || "").searchParams.get("name") || "";
     const res = await fetch(
       `https://api.magicthegathering.io/v1/cards?name=${name}`,
